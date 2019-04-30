58bc228ecf96c09861619e26aa44b1e1ac984219
kirjankarsten <45522425+kirjankarsten@users.noreply.github.com>
2019-04-30 12:48:21 +0200

    Script Changes
    BoardScript
    New Made Script

--- Chess/Assets/Scripts/boardScript.cs
+++ Chess/Assets/Scripts/boardScript.cs
@@ -1,40 +1,45 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class boardScript : MonoBehaviour
+public class BoardScript : MonoBehaviour
 {
-	Button b_1; Button b_2; Button b_3; Button b_4; Button b_5; Button b_6; Button b_7; Button b_8; Button b_9;
-	Button b_10; Button b_11; Button b_12; Button b_13; Button b_14; Button b_15; Button b_16; Button b_17; Button b_18;
-	Button b_19; Button b_20; Button b_21; Button b_22; Button b_23; Button b_24; Button b_25; Button b_26; Button b_27;
-	Button b_28; Button b_29; Button b_30; Button b_31; Button b_32; Button b_33; Button b_34; Button b_35; Button b_36;
-	Button b_37; Button b_38; Button b_39; Button b_40; Button b_41; Button b_42; Button b_43; Button b_44; Button b_45;
-	Button b_46; Button b_47; Button b_48; Button b_49; Button b_50; Button b_51; Button b_52; Button b_53; Button b_54;
-	Button b_55; Button b_56; Button b_57; Button b_58; Button b_59; Button b_60; Button b_61; Button b_62; Button b_63;
-	Button b_64; Button b_65; Button b_66; Button b_67; Button b_68; Button b_69; Button b_70; Button b_71; Button b_72;
-	Button b_73; Button b_74; Button b_75; Button b_76; Button b_77; Button b_78; Button b_79; Button b_80; Button b_81;
-
 	public Button[,] board;
+ 	public Button b1; public Button b2; public Button b3; public Button b4; public Button b5; public Button b6; public Button b7; public Button b8; public Button b9;
+    public Button b10; public Button b11; public Button b12; public Button b13; public Button b14; public Button b15; public Button b16; public Button b17; public Button b18;
+    public Button b19; public Button b20; public Button b21; public Button b22; public Button b23; public Button b24; public Button b25; public Button b26; public Button b27;
+    public Button b28; public Button b29; public Button b30; public Button b31; public Button b32; public Button b33; public Button b34; public Button b35; public Button b36;
+    public Button b37; public Button b38; public Button b39; public Button b40; public Button b41; public Button b42; public Button b43; public Button b44; public Button b45;
+    public Button b46; public Button b47; public Button b48; public Button b49; public Button b50; public Button b51; public Button b52; public Button b53; public Button b54;
+    public Button b55; public Button b56; public Button b57; public Button b58; public Button b59; public Button b60; public Button b61; public Button b62; public Button b63;
+    public Button b64; public Button b65; public Button b66; public Button b67; public Button b68; public Button b69; public Button b70; public Button b71; public Button b72;
+    public Button b73; public Button b74; public Button b75; public Button b76; public Button b77; public Button b78; public Button b79; public Button b80; public Button b81;
 
-    void Start() {
-	b_1 = new Button;
-    board = new Button[,] {
-		    {b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8, b_9},
-		    {b_10, b_11, b_12, b_13, b_14, b_15, b_16, b_17, b_18},
-		    {b_19, b_20, b_21, b_22, b_23, b_24, b_25, b_26, b_27},
-		    {b_28, b_29, b_30, b_31, b_32, b_33, b_34, b_35, b_36},
-		    {b_37, b_38, b_39, b_40, b_41, b_42, b_43, b_44, b_45},
-		    {b_46, b_47, b_48, b_49, b_50, b_51, b_52, b_53, b_54},
-		    {b_55, b_56, b_57, b_58, b_59, b_60, b_61, b_62, b_63},
-		    {b_64, b_65, b_66, b_67, b_68, b_69, b_70, b_71, b_72},
-		    {b_73, b_74, b_75, b_76, b_77, b_78, b_79, b_80, b_81}
+
+    public void Start()
+    {
+	    b1 = GetComponent<Button>();
+
+
+	    board = new [,] {
+		    {b1, b2, b3, b4, b5, b6, b7, b8, b9},
+		    {b10, b11, b12, b13, b14, b15, b16, b17, b18},
+		    {b19, b20, b21, b22, b23, b24, b25, b26, b27},
+		    {b28, b29, b30, b31, b32, b33, b34, b35, b36},
+		    {b37, b38, b39, b40, b41, b42, b43, b44, b45},
+		    {b46, b47, b48, b49, b50, b51, b52, b53, b54},
+		    {b55, b56, b57, b58, b59, b60, b61, b62, b63},
+		    {b64, b65, b66, b67, b68, b69, b70, b71, b72},
+		    {b73, b74, b75, b76, b77, b78, b79, b80, b81}
 	    };
 
-		for(int i = 0; i < 9; i++){
-			Debug.Log(board[i,i]);
-		}
+	    for (var i = 0; i < 9; i++)
+	    {
+		    Debug.Log(board[i, i].tag);
+	    }
     }
 
-	public Button[,] getBoard(){
-		return board;
-	}
+    public Button[,] GetBoard()
+    {
+	    return board;
+    }
 }

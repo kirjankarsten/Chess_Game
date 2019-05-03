f5008e90aa52dfe8237a750795a71096f9ba6030
kirjankarsten <45522425+kirjankarsten@users.noreply.github.com>
2019-05-03 13:28:59 +0200

    added UI buttons
    Added the buttons but they dont work smh

--- Chess/Assets/Scripts/PlayNewGame.cs
+++ Chess/Assets/Scripts/PlayNewGame.cs
@@ -4,7 +4,15 @@ public class PlayNewGame : MonoBehaviour
 {
     public void StartNewGame()
     {
+        Debug.Log("New Game Started");
         var boardScript = gameObject.GetComponent<BoardScript>();
         boardScript.ResetBoard();
+    }
+
+    public void ClickButtonTest()
+    {
+        Debug.Log("clicked");
+        DuringGame duringGame = GetComponent<DuringGame>();
+        duringGame.ButtonPressed();
     }
 }
